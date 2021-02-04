e7034b292329f9ef1dd81599954c3c874e86f33f
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2021-02-04 00:41:10 -0500

    add more sounds

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,21 @@ import { useState } from 'react';
 import PictureScroller from '../PictureScroller/PictureScroller';
 import shuffle from 'shuffle-array';
 import useSound from 'use-sound';
-import qoobeeSound from '../../sounds/qoob.mp3';
+// import qoob from '../../sounds/qoob5.mp3';
 import './App.css';
+
+const sounds = [
+  require('../../sounds/qoob0.mp3'),
+  require('../../sounds/qoob1.mp3'),
+  require('../../sounds/qoob2.mp3'),
+  require('../../sounds/qoob3.mp3'),
+  require('../../sounds/qoob4.mp3'),
+  require('../../sounds/qoob5.mp3'),
+  require('../../sounds/qoob6.mp3'),
+  require('../../sounds/qoob7.mp3'),
+  require('../../sounds/qoob8.mp3'),
+  require('../../sounds/qoob9.mp3')
+];
 
 const pics = shuffle([
   require('../../img/a.webp'),
@@ -26,16 +39,42 @@ const pics = shuffle([
 
 function App() {
   const [ spotlight, setSpotlight ] = useState(pics[Math.floor(Math.random() * pics.length)].default);
-  const [ playQoobee ] = useSound(qoobeeSound, { volume: 0.25 });
+  const [ playQoob0 ] = useSound(sounds[0].default, { volume: 0.25 });
+  const [ playQoob1 ] = useSound(sounds[1].default, { volume: 0.25 });
+  const [ playQoob2 ] = useSound(sounds[2].default, { volume: 0.25 });
+  const [ playQoob3 ] = useSound(sounds[3].default, { volume: 0.25 });
+  const [ playQoob4 ] = useSound(sounds[4].default, { volume: 0.25 });
+  const [ playQoob5 ] = useSound(sounds[5].default, { volume: 0.25 });
+  const [ playQoob6 ] = useSound(sounds[6].default, { volume: 0.25 });
+  const [ playQoob7 ] = useSound(sounds[7].default, { volume: 0.25 });
+  const [ playQoob8 ] = useSound(sounds[8].default, { volume: 0.25 });
+  const [ playQoob9 ] = useSound(sounds[9].default, { volume: 0.25 });
+
+  const playSounds = [
+    playQoob0,
+    playQoob1,
+    playQoob2,
+    playQoob3,
+    playQoob4,
+    playQoob5,
+    playQoob6,
+    playQoob7,
+    playQoob8,
+    playQoob9
+  ];
 
   const firstHalf = pics.slice(0, pics.length / 2);
   const secondHalf = pics.slice(pics.length / 2);
+
+  const handleClick = () => {
+    playSounds[Math.floor(Math.random() * playSounds.length)]();
+  };
 
   return (
     <div className="App">
       <PictureScroller pics={firstHalf} setSpotlight={setSpotlight} />
       <div className="App-Center">
-        <img src={spotlight} className="App-logo" alt="logo" onClick={playQoobee} />
+        <img src={spotlight} className="App-logo" alt="logo" onClick={handleClick} />
         <p>
           <code>qoobee.online</code>
         </p>
--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,28 +1,20 @@
 import './GalleryItem.css';
 import useSound from 'use-sound';
-import popDown from '../../sounds/pop-down.mp3';
-import popUp from '../../sounds/pop-up.mp3';
+import bloop from '../../sounds/bloo.mp3';
 
 function GalleryItem({ img, setSpotlight }) {
-  const [ playDown ] = useSound(popDown, { volume: 0.25 });
-  const [ playUp ] = useSound(popUp, { volume: 0.25 });
+  const [ playBloop ] = useSound(bloop, { volume: 0.25 });
 
   const src = img.default;
 
   const handleClick = () => {
+    playBloop();
     setSpotlight(src);
   };
 
   return (
     <div className="GalleryItem">
-      <img
-        onMouseDown={playDown}
-        onMouseUp={playUp}
-        className="GalleryItem-Pic"
-        src={src}
-        alt="qoobee"
-        onClick={handleClick}
-      />
+      <img className="GalleryItem-Pic" src={src} alt="qoobee" onClick={handleClick} />
     </div>
   );
 }
